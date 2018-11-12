56da098de23cee19af15b8ad9a79357b1bbf9593
evio <evio@vip.qq.com>
2018-11-12 12:50:15 +0800

    fix start bug

--- command/server/dev.js
+++ command/server/dev.js
@@ -3,7 +3,7 @@ const childProcess = require('child_process');
 const serverPath = path.resolve(__dirname, '../../server.js');
 
 module.exports = (cmd, configs) => {
-  const args = configs.__.filter(a => a.indexOf('-') === 0);
+  const args = configs.__.slice(2);
   args.unshift(serverPath);
   childProcess.spawn('node', args, {
     cwd: process.cwd(),
--- command/server/start.js
+++ command/server/start.js
@@ -4,7 +4,7 @@ const childProcess = require('child_process');
 const serverPath = path.resolve(__dirname, '../../server.js');
 
 module.exports = (cmd, configs) => {
-  const _args = configs.__.filter(a => a.indexOf('-') === 0);
+  const _args = configs.__.slice(2);
   const pkgfile = path.resolve(process.cwd(), 'package.json');
   const name = fs.existsSync(pkgfile) ? require(pkgfile).name : configs.name;
   const args = ['start', serverPath, '--name=' + (name || 'untitled'), '--', ..._args];
