269cc60f4961bc96dd5ed7724b7aa839ff827992
evio <evio@vip.qq.com>
2018-12-20 19:30:08 +0800

    update dev child process interval timer

--- command/server/dev.js
+++ command/server/dev.js
@@ -2,11 +2,27 @@ const path = require('path');
 const childProcess = require('child_process');
 const serverPath = path.resolve(__dirname, '../../server.js');
 
+let closing = false, timer = null;
+
 module.exports = (cmd, configs) => {
   const args = configs.__.slice(2);
   args.unshift(serverPath);
-  childProcess.spawn('node', args, {
+  const ls = childProcess.spawn('node', args, {
     cwd: process.cwd(),
     stdio: 'inherit'
   });
+  ls.on('close', () => {
+    clearInterval(timer);
+    process.exit(0);
+  });
 };
+
+process.on('SIGINT', close);
+process.on('SIGQUIT', close);
+process.on('SIGTERM', close);
+
+function close() {
+  if (closing) return;
+  closing = true;
+  timer = setInterval(() => {}, 10);
+}
